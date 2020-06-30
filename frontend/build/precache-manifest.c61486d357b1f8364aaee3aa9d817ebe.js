123e1e2a6f6737aba64ad24949fc015c915d520c
kiborgok <borgokalek14@gmail.com>
2020-06-30 23:35:08 +0300

    Edited

---
+++ frontend/build/precache-manifest.c61486d357b1f8364aaee3aa9d817ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f7b0f44c31b3a4547799de362a30bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5462ce17bd92983e8dd4",
+    "url": "/static/css/main.2086f105.chunk.css"
+  },
+  {
+    "revision": "4b5b4930c375559cf2fe",
+    "url": "/static/js/2.8067fa63.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.8067fa63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5462ce17bd92983e8dd4",
+    "url": "/static/js/main.846a2a50.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
-import path from "path";
+//import path from "path";
 import dotenv from 'dotenv'
 import { authRoutes, loanRoutes } from "./routes/index";
 dotenv.config();
@@ -10,35 +10,37 @@ dotenv.config();
 
 
 (async () => {
-  try {
+    try {
         await mongoose.connect(
-          process.env.MONGO_URI ||
+            process.env.MONGO_URI ||
             "mongodb+srv://kiborgok:Matesya1@uplift-tdpp1.mongodb.net/quickcredit?retryWrites=true&w=majority",
-          {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-          }
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false,
+            }
         );
         console.log("MongoDB connected");
 
         const app = express();
+        
         // Serve static assets if in production
         //if (process.env.NODE_ENV === "production") {
-          // Set static folder
-          app.use(express.static("frontend/build"));
+            // Set static folder
+            app.use(express.static("frontend/build"));
 
-          app.get("*", (req, res) => {
-            res.sendFile(
-              path.join(
-                __dirname,
-                "frontend",
-                "build",
-                "index.html"
-              )
-            );
-          });
-        
+            app.get("*", (req, res) => {
+                res.sendFile(
+                    path.join(
+                        __dirname,
+                        "frontend",
+                        "build",
+                        "index.html"
+                    )
+                );
+            });
+       // }
+       // */
         const port = process.env.PORT || 5000;
 
         app.disable("x-powered-by");
@@ -53,7 +55,7 @@ dotenv.config();
         apiRouter.use("/loans", loanRoutes);
 
         app.listen(port, () => console.log(`Listening on port ${port}`));
-      } catch (err) {
-    console.log(err);
-  }
+    } catch (err) {
+        console.log(err);
+    }
 })();
