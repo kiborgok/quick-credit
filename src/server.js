4daf993928ea749033dafd7be4b632491f9249aa
kiborgok <borgokalek14@gmail.com>
2020-07-04 15:44:39 +0300

    updated

--- src/server.js
+++ src/server.js
@@ -28,11 +28,7 @@ dotenv.config();
         // Serve static assets if in production
         if (process.env.NODE_ENV === "production") {
             // Set static folder
-            app.use(
-              express.static("frontend/build", {
-                setHeaders: res => res.type("json"),
-              })
-            );
+            app.use(express.static("frontend/build"));
 
             app.get("*", (req, res) => {
                 res.sendFile(
