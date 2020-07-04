e2b30f622d2af5f5becaf7b4423e18f555cedba4
kiborgok <borgokalek14@gmail.com>
2020-07-04 12:44:58 +0300

    updated

--- src/server.js
+++ src/server.js
@@ -28,11 +28,11 @@ dotenv.config();
         // Serve static assets if in production
         if (process.env.NODE_ENV === "production") {
             // Set static folder
-            app.use(express.static("frontend/build"));
+            app.use(express.static(path.resolve(__dirname, "frontend/build")));
 
-            app.get("*", (req, res) => {
+            app.get("/*", (req, res) => {
                 res.sendFile(
-                    path.join(
+                    path.resolve(
                         __dirname,
                         "/",
                         "../frontend/build",
