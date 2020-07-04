a36307d9b022741125b085068324d11afc37fec4
kiborgok <borgokalek14@gmail.com>
2020-07-04 17:00:06 +0300

    updated

--- src/server.js
+++ src/server.js
@@ -11,53 +11,46 @@ dotenv.config();
 
 (async () => {
     try {
-        await mongoose.connect(
-          process.env.MONGO_URI ||
-            "mongodb+srv://kiborgok:Matesya1@uplift-tdpp1.mongodb.net/quickcredit?retryWrites=true&w=majority",
-          //"mongodb://localhost:27017/uplift?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
-          {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-          }
-        );
-        console.log("MongoDB connected");
+          await mongoose.connect(
+            process.env.MONGO_URI ||
+              "mongodb+srv://kiborgok:Matesya1@uplift-tdpp1.mongodb.net/quickcredit?retryWrites=true&w=majority",
+            //"mongodb://localhost:27017/uplift?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+            {
+              useNewUrlParser: true,
+              useUnifiedTopology: true,
+              useFindAndModify: false,
+            }
+          );
+          console.log("MongoDB connected");
 
-        const app = express();
+          const app = express();
+          const port = process.env.PORT || 5000;
 
-        // Serve static assets if in production
-        if (process.env.NODE_ENV === "production") {
+          app.disable("x-powered-by");
+          app.use(express.json());
+          app.use(cors({ credentials: true }));
+          app.use(express.urlencoded({ extended: true }));
+          app.use(bodyParser.json());
+
+          // Serve static assets if in production
+          if (process.env.NODE_ENV === "production") {
             // Set static folder
             app.use(express.static("frontend/build"));
 
             app.get("*", (req, res) => {
-                res.sendFile(
-                    path.join(
-                        __dirname,
-                        "/",
-                        "../frontend/build",
-                        "index.html"
-                    )
-                );
+              res.sendFile(
+                path.join(__dirname, "/", "../frontend/build", "index.html")
+              );
             });
-        }
-        const port = process.env.PORT || 5000;
+          }
 
-        app.disable("x-powered-by");
-        app.use(
-            express.json(),
-          cors({ credentials: true }),
-          express.urlencoded({ extended: true }),
-          bodyParser.json()
-        );
+          const apiRouter = express.Router();
+          app.use("/api/v1", apiRouter);
+          apiRouter.use("/auth", authRoutes);
+          apiRouter.use("/loans", loanRoutes);
 
-        const apiRouter = express.Router();
-        app.use("/api/v1", apiRouter);
-        apiRouter.use("/auth", authRoutes);
-        apiRouter.use("/loans", loanRoutes);
-
-        app.listen(port, () => console.log(`Listening on port ${port}`));
-    } catch (err) {
+          app.listen(port, () => console.log(`Listening on port ${port}`));
+        } catch (err) {
         console.log(err);
     }
 })();
