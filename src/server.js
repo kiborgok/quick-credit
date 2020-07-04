75e320dadd71b4523ac85c40c8dbf43055937d05
kiborgok <borgokalek14@gmail.com>
2020-07-04 17:28:16 +0300

    updated

--- src/server.js
+++ src/server.js
@@ -28,8 +28,20 @@ dotenv.config();
     app.use(express.json());
     app.use(cors({ credentials: true }));
     app.use(express.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
-    res.set("content-type", "application/json");
+      app.use(bodyParser.json());
+      app.use(function (req, res, next) {
+        let allowedOrigins = ["*"]; // list of url-s
+        let origin = req.headers.origin;
+        if (allowedOrigins.indexOf(origin) > -1) {
+          res.setHeader("Access-Control-Allow-Origin", origin);
+        }
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        res.header("Access-Control-Expose-Headers", "Content-Disposition");
+        next();
+      });
 
     // Serve static assets if in production
     if (process.env.NODE_ENV === "production") {
