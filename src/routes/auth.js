382e69ffae97e16bf08e47967e5f199bfbc5cd5d
kiborgok <borgokalek14@gmail.com>
2020-07-04 17:38:55 +0300

    updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -30,7 +30,7 @@ authRoutes.get('/users/:userId', authenticateJWT, async (req, res) => {
         if (!admin) return res.json({ 'status': 403, 'error': 'Forbidden' });
         const { userId } = req.params;
         let foundUser = await User.findOne({ _id: userId }).populate('loan');
-        res.type("json");
+        res.set("Content-Type", "application/json");
         return res.json({ 'status': 200, 'data': foundUser });
     } catch (err) {
         return res.json({ 'status': 404, 'error': 'User not found' });
