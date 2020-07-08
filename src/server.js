ab1066e959e8be3e01d8ae5c069421bfc9f5f1fe
kiborgok <borgokalek14@gmail.com>
2020-07-09 02:00:26 +0300

    Updated

--- src/server.js
+++ src/server.js
@@ -42,7 +42,7 @@ dotenv.config();
     // }
     if (process.env.NODE_ENV === "production") {
       // Set static folder
-      app.use("/", express.static(path.join(__dirname,"client/build")), {
+      app.use("/", express.static("client/build"), {
         'setHeaders': function (res) {
           res.setHeader("Content-Type", "application/json");
         }
