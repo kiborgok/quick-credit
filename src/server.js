9917d46b27a471199b98032ecd11942973f09010
kiborgok <borgokalek14@gmail.com>
2020-07-09 10:33:11 +0300

    Updated

--- src/server.js
+++ src/server.js
@@ -44,7 +44,7 @@ dotenv.config();
       // Set static folder
       app.use("/", express.static(path.join(__dirname, "../client/build")));
 
-      app.get("/*", (req, res) => {
+      app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../client/build", "index.html"));
       });
     }
