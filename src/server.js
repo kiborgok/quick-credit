0567e38c21540603c9794c81646d2cdd31c7add0
kiborgok <borgokalek14@gmail.com>
2020-07-09 10:11:11 +0300

    Updated

--- src/server.js
+++ src/server.js
@@ -42,14 +42,10 @@ dotenv.config();
     // }
     if (process.env.NODE_ENV === "production") {
       // Set static folder
-      app.use("/", express.static("client/build"));
+      app.use("/", express.static(path.join(__dirname, "/../../client/build")));
 
       app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "/", "../client/build", "index.html"), {
-          setHeaders: function (res) {
-            res.set('Accept', 'application/json')
-          }
-        });
+        res.sendFile(path.join(__dirname, "/../../client/build", "index.html"));
       });
     }
 
